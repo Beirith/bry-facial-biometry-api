8e288ecf1c068ac7bc0c38640486e5ab7310e97c
Isaque Beirith <isaqueflorianobeirith@gmail.com>
2026-08-05 19:24:15 -0300

    Initial commit

---
+++ src/main/java/com/isaquebeirith/bry_facial_biometry_api/BryFacialBiometryApiApplication.java
@@ -0,0 +1,13 @@
+package com.isaquebeirith.bry_facial_biometry_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BryFacialBiometryApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BryFacialBiometryApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/isaquebeirith/bry_facial_biometry_api/BryFacialBiometryApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.isaquebeirith.bry_facial_biometry_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BryFacialBiometryApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
