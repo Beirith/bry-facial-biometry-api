08936425a5de2c8c09a6c57631f4cd34317972db
Isaque Beirith <isaqueflorianobeirith@gmail.com>
2026-08-06 01:27:52 -0300

    Feat: user e facial template models

---
+++ backend/src/main/java/com/isaquebeirith/bry_facial_biometry_api/model/FacialTemplate.java
@@ -0,0 +1,23 @@
+package com.isaquebeirith.bry_facial_biometry_api.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+@Entity
+@Table(name = "facial_templates")
+public class FacialTemplate {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne
+    @JoinColumn(name = "user_id", nullable = false, unique = true)
+    private User user;
+
+    @Lob
+    @Column(name = "feature_vector", nullable = false)
+    private byte[] featureVector;
+}
---
+++ backend/src/main/java/com/isaquebeirith/bry_facial_biometry_api/model/User.java
@@ -0,0 +1,25 @@
+package com.isaquebeirith.bry_facial_biometry_api.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String cpf;
+
+    @Lob
+    @Column(nullable = false)
+    private byte[] picture;
+}
