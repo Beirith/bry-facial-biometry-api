e8646a9dd52c62ad0118f8c32302fc36e84c4fa9
Isaque Beirith <isaqueflorianobeirith@gmail.com>
2026-08-06 02:49:07 -0300

    Feat: adiciona validações no user model

--- backend/src/main/java/com/isaquebeirith/bry_facial_biometry_api/model/User.java
+++ backend/src/main/java/com/isaquebeirith/bry_facial_biometry_api/model/User.java
@@ -1,6 +1,8 @@
 package com.isaquebeirith.bry_facial_biometry_api.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -14,12 +16,15 @@ public class User {
     private Long id;
 
     @Column(nullable = false)
+    @NotBlank(message = "É obrigatório informar o nome do usuário.")
     private String name;
 
     @Column(nullable = false, unique = true)
+    @NotBlank(message = "É obrigatório informar o CPF do usuário.")
     private String cpf;
 
     @Lob
     @Column(nullable = false)
+    @NotNull(message = "É obrigatório anexar uma foto do usuário.")
     private byte[] picture;
 }
