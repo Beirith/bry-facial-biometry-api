f686214508eff9d63c81b018d742f85f23c6eceb
Isaque Beirith <isaqueflorianobeirith@gmail.com>
2026-08-06 03:49:00 -0300

    Feat: remove validações do user model

--- backend/src/main/java/com/isaquebeirith/bry_facial_biometry_api/model/User.java
+++ backend/src/main/java/com/isaquebeirith/bry_facial_biometry_api/model/User.java
@@ -16,15 +16,12 @@ public class User {
     private Long id;
 
     @Column(nullable = false)
-    @NotBlank(message = "É obrigatório informar o nome do usuário.")
     private String name;
 
     @Column(nullable = false, unique = true)
-    @NotBlank(message = "É obrigatório informar o CPF do usuário.")
     private String cpf;
 
     @Lob
     @Column(nullable = false)
-    @NotNull(message = "É obrigatório anexar uma foto do usuário.")
     private byte[] picture;
 }
