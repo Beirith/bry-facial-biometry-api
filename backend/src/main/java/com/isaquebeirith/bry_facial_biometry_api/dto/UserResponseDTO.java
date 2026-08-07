162ec6fce1b58e1d5782904b5b4ef1ac29819dfc
Isaque Beirith <isaqueflorianobeirith@gmail.com>
2026-08-07 03:45:38 -0300

    Feat: método fromEntity UserResponseDTO

--- backend/src/main/java/com/isaquebeirith/bry_facial_biometry_api/dto/UserResponseDTO.java
+++ backend/src/main/java/com/isaquebeirith/bry_facial_biometry_api/dto/UserResponseDTO.java
@@ -1,5 +1,6 @@
 package com.isaquebeirith.bry_facial_biometry_api.dto;
 
+import com.isaquebeirith.bry_facial_biometry_api.model.User;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -9,4 +10,13 @@ public class UserResponseDTO {
     private Long id;
     private String name;
     private String cpf;
+
+    public static UserResponseDTO fromEntity(User user) {
+        UserResponseDTO dto = new UserResponseDTO();
+        dto.setId(user.getId());
+        dto.setName(user.getName());
+        dto.setCpf(user.getCpf());
+
+        return dto;
+    }
 }
