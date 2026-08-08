481e33658d483a7addf83b4b4b336d166e325f22
Isaque Beirith <isaqueflorianobeirith@gmail.com>
2026-08-08 06:07:53 -0300

    Feat: FacialTemplateComparator

---
+++ backend/src/main/java/com/isaquebeirith/bry_facial_biometry_api/biometry/comparation/FacialTemplateComparator.java
@@ -0,0 +1,28 @@
+package com.isaquebeirith.bry_facial_biometry_api.biometry.comparation;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class FacialTemplateComparator {
+    @Value("${biometry.similarity-threshold}")
+    private float threshold;
+
+    public float calculateSimilarity(float[] template1, float[] template2) {
+        float ret = 0.0f;
+        float mod1 = 0.0f;
+        float mod2 = 0.0f;
+
+        for (int i = 0; i < template1.length; ++i) {
+            ret += template1[i] * template2[i];
+            mod1 += template1[i] * template1[i];
+            mod2 += template2[i] * template2[i];
+        }
+
+        return (float) ((ret / Math.sqrt(mod1) / Math.sqrt(mod2) + 1) / 2.0f);
+    }
+
+    public boolean matches(float similarity) {
+        return similarity >= threshold;
+    }
+}
