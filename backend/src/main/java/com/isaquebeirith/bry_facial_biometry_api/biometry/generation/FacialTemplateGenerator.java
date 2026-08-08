7212f0bd8d97f72141d9056da8ace30b3d043614
Isaque Beirith <isaqueflorianobeirith@gmail.com>
2026-08-08 05:34:39 -0300

    Feat: exceções de detecção e extração

--- backend/src/main/java/com/isaquebeirith/bry_facial_biometry_api/biometry/generation/FacialTemplateGenerator.java
+++ backend/src/main/java/com/isaquebeirith/bry_facial_biometry_api/biometry/generation/FacialTemplateGenerator.java
@@ -5,8 +5,7 @@ import ai.djl.modality.cv.output.DetectedObjects;
 import ai.djl.modality.cv.output.Rectangle;
 import com.isaquebeirith.bry_facial_biometry_api.biometry.detection.FacialDetector;
 import com.isaquebeirith.bry_facial_biometry_api.biometry.recognition.FacialFeatureExtractor;
-import com.isaquebeirith.bry_facial_biometry_api.exception.MoreThanOneFaceDetectedException;
-import com.isaquebeirith.bry_facial_biometry_api.exception.NoFaceDetectedException;
+import com.isaquebeirith.bry_facial_biometry_api.exception.*;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
@@ -18,8 +17,15 @@ public class FacialTemplateGenerator {
     private final FacialDetector facialDetector;
     private final FacialFeatureExtractor facialFeatureExtractor;
 
-    public float[] generate(Image image) throws Exception {
-        DetectedObjects detections = facialDetector.detect(image);
+    public float[] generate(Image image)  {
+        DetectedObjects detections;
+
+        try {
+            detections = facialDetector.detect(image);
+        } catch (Exception e) {
+            throw new FacialDetectionException("Erro ao realizar detecção de rostos na imagem.");
+        }
+
         List<DetectedObjects.DetectedObject> faces = detections.items();
 
         if (faces.isEmpty()) {
@@ -33,7 +39,11 @@ public class FacialTemplateGenerator {
         DetectedObjects.DetectedObject face = faces.getFirst();
         Image croppedFace = cropFace(image, face);
 
-        return facialFeatureExtractor.extract(croppedFace);
+        try {
+            return facialFeatureExtractor.extract(croppedFace);
+        } catch (Exception e) {
+            throw new FacialExtractionException("Erro ao realizar extração de características na imagem.");
+        }
     }
 
     private Image cropFace(Image image, DetectedObjects.DetectedObject face) {
---
+++ backend/src/main/java/com/isaquebeirith/bry_facial_biometry_api/exception/FacialExtractionException.java
@@ -0,0 +1,7 @@
+package com.isaquebeirith.bry_facial_biometry_api.exception;
+
+public class FacialExtractionException extends RuntimeException {
+    public FacialExtractionException(String message) {
+        super(message);
+    }
+}
