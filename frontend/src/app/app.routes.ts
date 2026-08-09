c5f85710b4bd5860806bb8c7f85096c33c4abf34
Isaque Beirith <isaqueflorianobeirith@gmail.com>
2026-08-09 10:06:09 -0300

    Update app.routes.ts

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { UserCreate } from './presentation/users/user-create/user-create';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'users', component: UserList },
+  { path: '', redirectTo: 'users', pathMatch: 'full' }
+];
