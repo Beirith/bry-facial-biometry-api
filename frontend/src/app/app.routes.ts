7e111b86a869e57a9e2e20fddfdbb23d68b5a697
Isaque Beirith <isaqueflorianobeirith@gmail.com>
2026-08-09 19:00:46 -0300

    Feat: deleção

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,6 +2,12 @@ import { Routes } from '@angular/router';
 import { UserList } from './presentation/users/user-list/user-list';
 import { UserCreate } from './presentation/users/user-create/user-create';
 import { UserEdit } from './presentation/users/user-edit/user-edit';
+import {Routes} from '@angular/router';
+import {UserList} from './presentation/users/user-list/user-list';
+import {UserCreate} from './presentation/users/user-create/user-create';
+import {UserEdit} from './presentation/users/user-edit/user-edit';
+import {UserDetails} from './presentation/users/user-details/user-details';
+import {UserBatchCreate} from './presentation/users/user-batch-create/user-batch-create';
 
 export const routes: Routes = [
   { path: 'users', component: UserList },
@@ -9,4 +15,9 @@ export const routes: Routes = [
   { path: 'users/edit/:id', component: UserEdit },
   { path: 'users/:id', component: UserDetails },
   { path: '', redirectTo: 'users', pathMatch: 'full' }
+  {path: 'users', component: UserList},
+  {path: 'users/create', component: UserCreate},
+  {path: 'users/batch-create', component: UserBatchCreate},
+  {path: 'users/edit/:id', component: UserEdit},
+  {path: 'users/:id', component: UserDetails},
 ];
