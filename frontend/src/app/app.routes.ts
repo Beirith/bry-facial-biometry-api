9eaf838802af788808bcb1c76254cf131f12d63f
Isaque Beirith <isaqueflorianobeirith@gmail.com>
2026-08-09 10:06:25 -0300

    Feat: user create

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
+import { UserList } from './presentation/users/user-list/user-list';
 import { UserCreate } from './presentation/users/user-create/user-create';
 
 export const routes: Routes = [
   { path: 'users', component: UserList },
+  { path: 'users/create', component: UserCreate },
   { path: '', redirectTo: 'users', pathMatch: 'full' }
 ];
---
+++ frontend/src/app/presentation/users/user-create/user-create.ts
@@ -0,0 +1,67 @@
+import {Component, inject, signal} from '@angular/core';
+import {CommonModule} from '@angular/common'
+import {FormBuilder, FormGroup, Validators, ReactiveFormsModule} from '@angular/forms';
+import {Router} from '@angular/router';
+import {UserService} from '../../../data/services/user.service';
+
+@Component({
+  selector: 'app-user-create',
+  imports: [CommonModule, ReactiveFormsModule],
+  templateUrl: './user-create.html',
+  styleUrl: './user-create.css'
+})
+export class UserCreate {
+  private fb = inject(FormBuilder);
+
+  userForm: FormGroup = this.fb.group({
+    name: ['', [Validators.required]],
+    cpf: ['', [Validators.required]],
+  });
+
+  selectedFile: File | null = null;
+  error = signal<string | null>(null);
+  submitting = signal(false);
+
+  constructor(
+    private userService: UserService,
+    private router: Router) {}
+
+  onPictureSelected(event: Event): void {
+    const input = event.target as HTMLInputElement;
+
+    if (input.files && input.files.length > 0) {
+      this.selectedFile = input.files[0];
+    }
+  }
+
+  onSubmit(): void {
+    if (this.userForm.invalid) {
+      this.error.set('Preencha todos os campos.');
+      return;
+    }
+
+    if (!this.selectedFile) {
+      this.error.set('Anexe uma foto.');
+      return;
+    }
+
+    const formData = new FormData();
+    formData.append('name', this.userForm.value.name!);
+    formData.append('cpf', this.userForm.value.cpf!);
+    formData.append('picture', this.selectedFile);
+
+    this.submitting.set(true);
+    this.error.set(null);
+
+    this.userService.create(formData).subscribe({
+      next: () => {
+        this.submitting.set(false);
+        this.router.navigate(['/users']);
+      },
+      error: (err) => {
+        this.submitting.set(false);
+        this.error.set(err.error?.message || 'Erro ao cadastrar usuário.');
+      }
+    });
+  }
+}
