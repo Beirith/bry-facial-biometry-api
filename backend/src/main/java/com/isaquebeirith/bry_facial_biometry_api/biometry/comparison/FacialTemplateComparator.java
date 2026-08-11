70dfbe49ee6f1c48b09a075da3b45db90036b9c0
Isaque Beirith <isaqueflorianobeirith@gmail.com>
2026-08-11 06:55:17 -0300

    Feat: ajusta nome pacote biometria

--- backend/src/main/java/com/isaquebeirith/bry_facial_biometry_api/biometry/comparison/FacialTemplateComparator.java
+++ backend/src/main/java/com/isaquebeirith/bry_facial_biometry_api/biometry/comparison/FacialTemplateComparator.java
@@ -1,4 +1,4 @@
-package com.isaquebeirith.bry_facial_biometry_api.biometry.comparation;
+package com.isaquebeirith.bry_facial_biometry_api.biometry.comparison;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
